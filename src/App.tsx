032e67e359ae8d220ca37d63fe4635cdc338f1a3
koyashiro <6698252+koyashiro@users.noreply.github.com>
2023-03-30 23:15:39 +0900

    fix: fix style

--- src/App.tsx
+++ src/App.tsx
@@ -5,23 +5,29 @@ function App() {
   const [enabled, setEnabled] = useState(false);
 
   return (
-    <div className="container mx-auto max-w-lg">
-      <h1 className="mt-1 text-4xl font-bold">alt-ime</h1>
+    <div className="h-screen bg-red-50">
+      <div className="container mx-auto max-w-lg">
+        <h1 className="text-4xl font-bold">alt-ime</h1>
 
-      <hr className="mt-3" />
+        <hr className="mt-3" />
 
-      <div className="mt-2">
-        <div className="flex flex-row items-center justify-between">
-          <div>
+        <div className="mt-2">
+          <div className="flex flex-row items-center justify-between">
             <div>
-              <h2 className="text-xl font-semibold">Altキーの監視</h2>
+              <div>
+                <h2 className="text-xl font-semibold">Altキーの監視</h2>
+              </div>
+              <div className="text-gray-700">
+                Altキーを監視してIMEを制御する。
+              </div>
             </div>
-            <div className="text-gray-700">
-              Altキーを監視してIMEを制御する。
+            <div className="mt-2">
+              <ToggleSwitch
+                checked={enabled}
+                label={""}
+                onChange={setEnabled}
+              />
             </div>
-          </div>
-          <div className="mt-2">
-            <ToggleSwitch checked={enabled} label={""} onChange={setEnabled} />
           </div>
         </div>
       </div>
