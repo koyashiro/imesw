e7d9e8fc2fd316960e90d2a74e1421d647b60f85
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-02-22 00:31:02 +0900

    refactor: improve setup

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,7 +12,7 @@ use std::{process, sync::Arc};
 use tauri::{
     CustomMenuItem, Manager, PhysicalSize, SystemTray,
     SystemTrayEvent::{DoubleClick, LeftClick, MenuItemClick},
-    SystemTrayMenu, Window,
+    SystemTrayMenu,
     WindowEvent::{CloseRequested, Resized},
 };
 
@@ -21,6 +21,10 @@ use crate::ime::ImeActivatorImpl;
 use crate::keyboard::KeyboardImpl;
 
 fn main() {
+    const OPEN_CUSTOM_MENU_ITEM_ID: &str = "open";
+    const IS_RUNNING_CUSTOM_MENU_ITEM_ID: &str = "is_running";
+    const QUIT_CUSTOM_MENU_ITEM_ID: &str = "quit";
+
     tauri::Builder::default()
         .setup({
             move |app| {
@@ -28,6 +32,7 @@ fn main() {
 
                 let config_manager: Arc<dyn ConfigManager> =
                     Arc::new(ConfigManagerImpl::new(app_handle));
+                config_manager.load_or_init()?;
 
                 hook::init(
                     config_manager.clone(),
@@ -56,48 +61,6 @@ fn main() {
                     }
                 });
 
-                const OPEN_CUSTOM_MENU_ITEM_ID: &str = "open";
-                const IS_RUNNING_CUSTOM_MENU_ITEM_ID: &str = "is_running";
-                const QUIT_CUSTOM_MENU_ITEM_ID: &str = "quit";
-                SystemTray::new()
-                    .with_menu(
-                        SystemTrayMenu::new()
-                            .add_item(CustomMenuItem::new(OPEN_CUSTOM_MENU_ITEM_ID, "Open"))
-                            .add_item(CustomMenuItem::new(
-                                IS_RUNNING_CUSTOM_MENU_ITEM_ID,
-                                "Active",
-                            ))
-                            .add_item(CustomMenuItem::new(QUIT_CUSTOM_MENU_ITEM_ID, "Quit")),
-                    )
-                    .on_event({
-                        let open_window = move |w: &Window| {
-                            w.unminimize().expect("Failed to unminimize the window");
-                            w.show().expect("Failed to show the window");
-                            w.set_focus()
-                                .expect("Failed to set the focus to the window");
-                        };
-                        let config_manager = config_manager.clone();
-                        move |e| match e {
-                            LeftClick { .. } | DoubleClick { .. } => open_window(&main_window),
-                            MenuItemClick { id, .. } => match id.as_str() {
-                                OPEN_CUSTOM_MENU_ITEM_ID => open_window(&main_window),
-                                IS_RUNNING_CUSTOM_MENU_ITEM_ID => {
-                                    config_manager
-                                        .toggle_is_running()
-                                        .expect("Failed to set is_running");
-                                }
-                                QUIT_CUSTOM_MENU_ITEM_ID => {
-                                    main_window.close().expect("Failed to close the window")
-                                }
-                                _ => (),
-                            },
-                            _ => (),
-                        }
-                    })
-                    .build(app)?;
-
-                config_manager.load_or_init()?;
-
                 Ok(())
             }
         })
@@ -107,6 +70,49 @@ fn main() {
             command::set_activate_key,
             command::set_deactivate_key,
         ])
+        .system_tray(
+            SystemTray::new().with_menu(
+                SystemTrayMenu::new()
+                    .add_item(CustomMenuItem::new(OPEN_CUSTOM_MENU_ITEM_ID, "Open"))
+                    .add_item(CustomMenuItem::new(
+                        IS_RUNNING_CUSTOM_MENU_ITEM_ID,
+                        "Active",
+                    ))
+                    .add_item(CustomMenuItem::new(QUIT_CUSTOM_MENU_ITEM_ID, "Quit")),
+            ),
+        )
+        .on_system_tray_event(|app_handle, event| {
+            let main_window_label = &app_handle.config().tauri.windows[0].label;
+            let main_window = app_handle.get_window(main_window_label).unwrap();
+            let main_window = &main_window;
+            let open_window = move || {
+                main_window
+                    .unminimize()
+                    .expect("Failed to unminimize the window");
+                main_window.show().expect("Failed to show the window");
+                main_window
+                    .set_focus()
+                    .expect("Failed to set the focus to the window");
+            };
+            let close_window = move || main_window.close().expect("Failed to close the window");
+            let config_manager = app_handle.state::<Arc<dyn ConfigManager>>();
+            match event {
+                LeftClick { .. } | DoubleClick { .. } => open_window(),
+                MenuItemClick { id, .. } => match id.as_str() {
+                    OPEN_CUSTOM_MENU_ITEM_ID => open_window(),
+                    IS_RUNNING_CUSTOM_MENU_ITEM_ID => {
+                        config_manager
+                            .toggle_is_running()
+                            .expect("Failed to set is_running");
+                    }
+                    QUIT_CUSTOM_MENU_ITEM_ID => {
+                        close_window();
+                    }
+                    _ => (),
+                },
+                _ => (),
+            }
+        })
         .run(tauri::generate_context!())
         .unwrap_or_else(|e| {
             eprintln!("Failed to run tauri application: {e}");
