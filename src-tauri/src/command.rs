f2399865853136a95b772913c7b885592d3f8d55
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-02-26 13:12:23 +0900

    feat: add tray module (#33)

--- src-tauri/src/command.rs
+++ src-tauri/src/command.rs
@@ -5,6 +5,7 @@ use tauri::{InvokeError, State};
 use crate::{
     config::{Config, ConfigManager},
     keyboard::Key,
+    tray::SystemTrayUpdater,
 };
 
 #[tauri::command]
@@ -16,22 +17,31 @@ pub fn get_config(config_manager: State<Arc<dyn ConfigManager>>) -> Result<Confi
 #[tauri::command]
 pub fn set_is_running(
     config_manager: State<Arc<dyn ConfigManager>>,
+    system_tray_updater: State<Arc<dyn SystemTrayUpdater>>,
     is_running: bool,
 ) -> Result<(), InvokeError> {
     config_manager
         .set_is_running(is_running)
         .map_err(InvokeError::from_anyhow)?;
-
+    let config = config_manager.config().map_err(InvokeError::from_anyhow)?;
+    system_tray_updater
+        .update(&config)
+        .map_err(InvokeError::from_anyhow)?;
     Ok(())
 }
 
 #[tauri::command]
 pub fn set_activate_key(
     config_manager: State<Arc<dyn ConfigManager>>,
+    system_tray_updater: State<Arc<dyn SystemTrayUpdater>>,
     key: Key,
 ) -> Result<(), InvokeError> {
     config_manager
         .set_activate_key(key)
+        .map_err(InvokeError::from_anyhow)?;
+    let config = config_manager.config().map_err(InvokeError::from_anyhow)?;
+    system_tray_updater
+        .update(&config)
         .map_err(InvokeError::from_anyhow)?;
     Ok(())
 }
@@ -39,10 +49,15 @@ pub fn set_activate_key(
 #[tauri::command]
 pub fn set_deactivate_key(
     config_manager: State<Arc<dyn ConfigManager>>,
+    system_tray_updater: State<Arc<dyn SystemTrayUpdater>>,
     key: Key,
 ) -> Result<(), InvokeError> {
     config_manager
         .set_deactivate_key(key)
+        .map_err(InvokeError::from_anyhow)?;
+    let config = config_manager.config().map_err(InvokeError::from_anyhow)?;
+    system_tray_updater
+        .update(&config)
         .map_err(InvokeError::from_anyhow)?;
     Ok(())
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -6,63 +6,61 @@ mod config;
 mod hook;
 mod ime;
 mod keyboard;
+mod tray;
 
 use std::{process, sync::Arc};
 
 use tauri::{
-    CustomMenuItem, Manager, PhysicalSize, SystemTray,
-    SystemTrayEvent::{DoubleClick, LeftClick, MenuItemClick},
-    SystemTrayMenu,
+    Manager, PhysicalSize,
     WindowEvent::{CloseRequested, Resized},
 };
+use tray::SystemTrayUpdaterImpl;
 
 use crate::config::{ConfigManager, ConfigManagerImpl};
 use crate::ime::ImeActivatorImpl;
 use crate::keyboard::KeyboardImpl;
 
 fn main() {
-    const OPEN_CUSTOM_MENU_ITEM_ID: &str = "open";
-    const IS_RUNNING_CUSTOM_MENU_ITEM_ID: &str = "is_running";
-    const QUIT_CUSTOM_MENU_ITEM_ID: &str = "quit";
-
     tauri::Builder::default()
-        .setup({
-            move |app| {
-                let app_handle = app.handle();
+        .system_tray(tray::system_tray())
+        .setup(move |app| {
+            let config = config::read_or_default();
 
-                let config_manager: Arc<dyn ConfigManager> =
-                    Arc::new(ConfigManagerImpl::new(app_handle));
-                config_manager.load_or_init()?;
+            let system_tray_handle = app
+                .tray_handle()
+                .get_item(tray::IS_RUNNING_CUSTOM_MENU_ITEM_ID);
+            let system_tray_updater = SystemTrayUpdaterImpl::new(system_tray_handle);
+            let config_manager: Arc<dyn ConfigManager> =
+                Arc::new(ConfigManagerImpl::new(config, system_tray_updater));
 
-                hook::init(
-                    config_manager.clone(),
-                    Box::new(ImeActivatorImpl),
-                    Box::new(KeyboardImpl),
-                )?;
+            hook::init(
+                config_manager.clone(),
+                Box::new(ImeActivatorImpl),
+                Box::new(KeyboardImpl),
+            )?;
 
-                app.manage(config_manager.clone());
+            app.manage(config_manager);
 
-                let main_window_label = &app.config().tauri.windows[0].label;
-                let main_window = app.get_window(main_window_label).unwrap();
-                main_window.on_window_event({
-                    let w = main_window.clone();
-                    move |e| match e {
-                        CloseRequested { api, .. } => {
-                            w.hide().expect("Failed to hide the window");
-                            api.prevent_close();
-                        }
-                        Resized(PhysicalSize {
-                            width: 0,
-                            height: 0,
-                        }) => {
-                            w.hide().expect("Failed to hide the window");
-                        }
-                        _ => (),
+            let main_window_label = &app.config().tauri.windows[0].label;
+            let main_window = app.get_window(main_window_label).unwrap();
+            main_window.on_window_event({
+                let w = main_window.clone();
+                move |e| match e {
+                    CloseRequested { api, .. } => {
+                        w.hide().expect("Failed to hide the window");
+                        api.prevent_close();
                     }
-                });
+                    Resized(PhysicalSize {
+                        width: 0,
+                        height: 0,
+                    }) => {
+                        w.hide().expect("Failed to hide the window");
+                    }
+                    _ => (),
+                }
+            });
 
-                Ok(())
-            }
+            Ok(())
         })
         .invoke_handler(tauri::generate_handler![
             command::get_config,
@@ -70,49 +68,7 @@ fn main() {
             command::set_activate_key,
             command::set_deactivate_key,
         ])
-        .system_tray(
-            SystemTray::new().with_menu(
-                SystemTrayMenu::new()
-                    .add_item(CustomMenuItem::new(OPEN_CUSTOM_MENU_ITEM_ID, "Open"))
-                    .add_item(CustomMenuItem::new(
-                        IS_RUNNING_CUSTOM_MENU_ITEM_ID,
-                        "Active",
-                    ))
-                    .add_item(CustomMenuItem::new(QUIT_CUSTOM_MENU_ITEM_ID, "Quit")),
-            ),
-        )
-        .on_system_tray_event(|app_handle, event| {
-            let main_window_label = &app_handle.config().tauri.windows[0].label;
-            let main_window = app_handle.get_window(main_window_label).unwrap();
-            let main_window = &main_window;
-            let open_window = move || {
-                main_window
-                    .unminimize()
-                    .expect("Failed to unminimize the window");
-                main_window.show().expect("Failed to show the window");
-                main_window
-                    .set_focus()
-                    .expect("Failed to set the focus to the window");
-            };
-            let close_window = move || main_window.close().expect("Failed to close the window");
-            let config_manager = app_handle.state::<Arc<dyn ConfigManager>>();
-            match event {
-                LeftClick { .. } | DoubleClick { .. } => open_window(),
-                MenuItemClick { id, .. } => match id.as_str() {
-                    OPEN_CUSTOM_MENU_ITEM_ID => open_window(),
-                    IS_RUNNING_CUSTOM_MENU_ITEM_ID => {
-                        config_manager
-                            .toggle_is_running()
-                            .expect("Failed to set is_running");
-                    }
-                    QUIT_CUSTOM_MENU_ITEM_ID => {
-                        close_window();
-                    }
-                    _ => (),
-                },
-                _ => (),
-            }
-        })
+        .on_system_tray_event(tray::system_tray_event)
         .run(tauri::generate_context!())
         .unwrap_or_else(|e| {
             eprintln!("Failed to run tauri application: {e}");
